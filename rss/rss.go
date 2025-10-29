68f85efd7c23ab665076aaaacfe5e12eea1f411b
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-10-28 21:17:03 -0400

    feat: add starting code for rss structs and fetch func

---
+++ rss/rss.go
@@ -0,0 +1,24 @@
+package rss
+
+import "context"
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func FetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	// Implementation for fetching and parsing the RSS feed goes here.
+	return nil, nil
+}
