ea4f8ccad9694b58813ee4d28e1fdc80fe8c9e04
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-11-01 21:35:42 -0400

    feat: add new posts table

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE posts(
+    id TEXT PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT,
+    url TEXT UNIQUE NOT NULL,
+    description TEXT,
+    published_at TIMESTAMP WITH TIME ZONE,
+    feed_id TEXT NOT NULL,
+    FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
