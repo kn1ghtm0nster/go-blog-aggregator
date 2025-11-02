d74fe8ef1472dd6301b2e400787f5c47d52180c1
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-11-01 20:03:21 -0400

    feat: update db for marking feeds as fetched

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -40,3 +40,14 @@ WHERE feed_follows.user_id = $1;
 -- name: UnfollowFeed :exec
 DELETE FROM feed_follows
 WHERE user_id = $1 AND feed_id = $2;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = CURRENT_TIMESTAMP,
+    updated_at = CURRENT_TIMESTAMP
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
